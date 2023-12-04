1c21a9edb86eb3d454d42182ec965fa172dc9dc5
grecolive <48304759+grecolive@users.noreply.github.com>
2023-12-04 19:24:11 -0400

    Last modified

--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -17,6 +17,19 @@
                 </div>
             </div>
             <div class="mt-6 bg-white shadow-sm rounded-lg divide-y">
+                @if($chirps->isEmpty())
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        <p class="text-lg text-gray-900">{{__('No Chirps yet.')}}</p>
+                    </div>
+                @else
+                    <div class="p-6 text-gray-900 dark:text-gray-100 flex gap-2 space-x-2 justify-items-center align-middle">
+                        <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-gray-600 -scale-x-100" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 01.865-.501 48.172 48.172 0 003.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z" />
+                          </svg>
+
+                        <p class="text-lg text-gray-900">{{Auth::user()->chirps()->count()}} - User Chirps</p>
+                    </div>
+                @endif
                 @foreach ($chirps as $chirp)
                     @canany(['update', 'view', 'delete'], $chirp)
                     <div class="p-6 flex space-x-2">
@@ -31,11 +44,11 @@
                                     @unless($chirp->created_at->eq($chirp->updated_at))
                                         <small class="ml-2 text-sm text-gray-600">&middot; {{ __('Edited') }}</small>
                                     @endunless
-                                    
+
                                 </div>
                             </div>
                             <p class="mt-4 text-lg text-gray-900">{{ $chirp->message }}</p>
-                        </div>                           
+                        </div>
                         <x-dropdown>
                             <x-slot name="trigger">
                                 <button class="text-gray-600 hover:text-gray-900">
